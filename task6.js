e17b5948083e2bdfcf0b3cd809b28b94379748b2
Tushar_kherde <tusharkherde83@gmail.com>
2025-08-15 16:47:33 +0530

    task 6 is completed with this task

---
+++ task6.js
@@ -0,0 +1,19 @@
+setTimeout(function(){
+    console.log("HELLO");
+}, 3000);
+
+setTimeout(function(){
+    console.log("MY");
+}, 3000);
+
+setTimeout(function(){
+    console.log("NAME");
+}, 3000);
+
+setTimeout(function(){
+    console.log("IS");
+}, 3000);
+
+setTimeout(function(){
+    console.log("TUSHAR");
+}, 3000);
