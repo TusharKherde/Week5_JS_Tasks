48d6378696a8b38d0ef7647b40ae766ba090aa72
Tushar_kherde <tusharkherde83@gmail.com>
2025-08-15 16:28:49 +0530

    task 4 is completed with this file

---
+++ task4.js
@@ -0,0 +1,5 @@
+let input = document.querySelector("input");
+let button = document.querySelector("button");
+button.addEventListener("click", function() {
+    alert("length of input is  " + input.value.length);
+});
