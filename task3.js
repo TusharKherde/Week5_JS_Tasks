71cf6c6003ad5db9865b49a987942c00f9fd1b45
Tushar_kherde <tusharkherde83@gmail.com>
2025-08-15 15:59:26 +0530

    task3 is completed with this file

---
+++ task3.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", function() {
+    document.body.style.backgroundColor = "lightblue";
+});
+btn.addEventListener("dblclick", function() {
+    document.body.style.backgroundColor = "lightgreen";
+});
